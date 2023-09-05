77f3d142b1f27621109170a1169ca90eb559becc
pirate1111 <48500307+pirate1111@users.noreply.github.com>
2023-09-05 20:05:07 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18676 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scann = new Scanner(System.in);
+		int t;
+		t = scann.nextInt();
+		int[] a = new int[t];
+		int[] b = new int[t];
+		int[] result = new int[t];
+		
+		
+		for(int i=0; i<t; i++) {
+			a[i] = scann.nextInt();
+			b[i] = scann.nextInt();
+		}
+		
+		for(int i=0; i<t; i++) {
+			result[i] = a[i] + b[i];
+			System.out.print("Case #"+(i+1)+": "+result[i]+"\n");
+		}
+	}
+}
