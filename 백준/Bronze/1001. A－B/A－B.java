5ff266a9c066d37a17e1490a70137b42349c4ecf
pirate1111 <48500307+pirate1111@users.noreply.github.com>
2023-09-11 16:29:03 +0900

    [Bronze V] Title: A-B, Time: 216 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		int a, b;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+		
+		System.out.print(a-b);
+
+	}
+}
