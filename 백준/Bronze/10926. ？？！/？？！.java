540480c22b29de6ee38c4494682859d3c915d642
pirate1111 <48500307+pirate1111@users.noreply.github.com>
2023-09-12 09:29:11 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		String a;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		a = scan.nextLine();
+		
+		System.out.println(a+"??!");
+	}
+}
