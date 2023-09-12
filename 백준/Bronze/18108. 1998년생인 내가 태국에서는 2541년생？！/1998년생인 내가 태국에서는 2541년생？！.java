981b9917daf314f5ac8930d78ed07ba07020b475
pirate1111 <48500307+pirate1111@users.noreply.github.com>
2023-09-12 09:45:07 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		int year;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		year = scan.nextInt();
+		
+		System.out.println(year-543);
+	}
+}
