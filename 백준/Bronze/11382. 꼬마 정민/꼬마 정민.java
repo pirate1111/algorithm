a95afe3b5a9bf32e2f6f2b2ab8e9937935fdbab9
pirate1111 <48500307+pirate1111@users.noreply.github.com>
2023-09-13 12:44:05 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scan = new Scanner(System.in);
+		
+		long a, b, c;
+		
+		a = scan.nextLong();
+		b = scan.nextLong();
+		c = scan.nextLong();
+		
+		System.out.println(a+b+c);
+	}
+}
