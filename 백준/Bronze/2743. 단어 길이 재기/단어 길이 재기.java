53da2a0ae542eed748efaaf23c8a504b27c0b0e1
pirate1111 <48500307+pirate1111@users.noreply.github.com>
2023-09-26 17:15:48 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String word = scan.nextLine();
+		
+		System.out.println(word.length());
+	}
+}
